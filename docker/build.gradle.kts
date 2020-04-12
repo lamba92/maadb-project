53705d741a23218462486d26b399c0e95c9f5b14
Lamberto Basti <basti.lamberto@gmail.com>
2020-04-12 17:28:26 +0200

    well well, going CI! Fingers crossed 🤞🤞 v10

--- docker/build.gradle.kts
+++ docker/build.gradle.kts
@@ -42,7 +42,7 @@ if (isDockerInstalled && System.getenv("CI")?.toBoolean() == true) {
                 "build",
                 "-t",
                 "lamba92/jupyter-kotlin:$version",
-                "--platform=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6,linux/arm/v8",
+                "--platform=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6",
                 folder.absolutePath
             )
         }
@@ -64,7 +64,7 @@ if (isDockerInstalled && System.getenv("CI")?.toBoolean() == true) {
                 "build",
                 "-t",
                 "lamba92/jupyter-kotlin:$version",
-                "--platform=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6,linux/arm/v8",
+                "--platform=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6",
                 folder.absolutePath,
                 "--push"
             )
