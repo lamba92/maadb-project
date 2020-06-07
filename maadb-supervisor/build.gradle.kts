c6a1a659a03f94ce22bac6d9be500ac3ee03d042
lamba <basti.lamberto@gmail.com>
2020-06-07 17:43:46 +0200

    merged feeders and supervisor! now the cluster will self feed on startup

--- maadb-supervisor/build.gradle.kts
+++ maadb-supervisor/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
     kotlin("plugin.serialization")
 }
 
+// workaround for https://youtrack.jetbrains.com/issue/KT-38165
+val copySourcesWorkaround by tasks.creating(Copy::class) {
+    from("$rootDir/core/src/jvmMain/resources")
+    into("$projectDir/src/main/resources")
+}
+
+kotlin.target.compilations["main"].compileKotlinTask.dependsOn(copySourcesWorkaround)
+
 application {
     mainClassName = "edu.unito.maadb.mongosupervisor.MainKt"
 }
